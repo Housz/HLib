05e9064cf1aa09b617785512328291e6105c5ee6
Housz <1215123803@qq.com>
2017-11-17 10:46:08 +0800

    201711162226

--- vector/vector_shrink.h
+++ vector/vector_shrink.h
@@ -7,9 +7,20 @@
 
 #include "vector.h"
 
-
-template <class T> void Vector<T>::shrink()
+//装填因子（_size/_capacity）过小时（设为25%）压缩向量所占空间（容量减半）
+template<class T>
+void Vector<T>::shrink()
 {
+    if (_capacity < DEFAULT_CAPACITY << 1)return;//不会收缩到DEFAULT_CAPACITY以下
+    if (_size << 2 > _capacity) return;//装填因子25%为界
 
+    T* oldElem = _elem;
+    _elem = new T[_capacity >>= 1];//容量减半
+    for (int i = 0; i < _size; ++i)//复制原向量内容
+    {
+        _elem[i] = oldElem[i];
+    }
+    delete [] oldElem;
 }
+
 #endif //HLIB_VECTOR_SHRINK_H
