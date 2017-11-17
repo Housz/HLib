2649c2d284821d6d90b794b1e05b7ead0507e029
Housz <1215123803@qq.com>
2017-11-17 08:18:11 +0800

    201711162226

--- vector/vector_expand.h
+++ vector/vector_expand.h
@@ -12,18 +12,18 @@ template<class T>
 void Vector<T>::expand()
 {
     if (_size < _capacity)return;//向量未满时，不必扩容
-    if (_capacity < DEFAULT_CAPACITY)//
+    if (_capacity < DEFAULT_CAPACITY)//不低于最小容量
         _capacity = DEFAULT_CAPACITY;
 
     T* oldElem = _elem;
-        _elem = new T[_capacity << = 1];
+        _elem = new T[_capacity <<= 1];//容量加倍
 
-    for (int i = 0; i < _size; ++i)
+    for (int i = 0; i < _size; ++i) //复制原向量内容（T为基本类型，或已重载赋值操作符'='）
     {
         _elem[i] = oldElem[i];
     }
 
-    delete [] oldElem;
+    delete [] oldElem;//释放原空间
 }
 
 #endif //HLIB_VECTOR_EXPAND_H
