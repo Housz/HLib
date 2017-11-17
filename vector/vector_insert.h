4f7812b04c5245213ad0539a7d35fab1ecff7c65
Housz <1215123803@qq.com>
2017-11-17 08:23:01 +0800

    201711162226

--- vector/vector_insert.h
+++ vector/vector_insert.h
@@ -5,10 +5,20 @@
 #ifndef HLIB_VECTOR_INSERT_H
 #define HLIB_VECTOR_INSERT_H
 
-template <class T>
+#include "vector.h"
+
+template<class T>
 Rank Vector<T>::insert(Rank r, T const &e)
 {
-    expand();
+    expand();//若有必要则扩容
 
+    for (int i = _size; i > r; i--)//自后向前，r后继元素依次后移一单位
+    {
+        _elem[i] = _elem[i - 1];
+    }
+    _elem[r] = e;//在r位置插入e
+    _size++;//更新向量容量
+    return r;//返回秩
 }
+
 #endif //HLIB_VECTOR_INSERT_H
