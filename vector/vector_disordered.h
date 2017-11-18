a1cdaed85d00721f1f3f654482911cf1a9830dda
Housz <1215123803@qq.com>
2017-11-18 10:36:42 +0800

    20171118

--- vector/vector_disordered.h
+++ vector/vector_disordered.h
@@ -5,16 +5,22 @@
 #ifndef HLIB_VECTOR_DISORDERED_H
 #define HLIB_VECTOR_DISORDERED_H
 
+#include "vector.h"
+
 #pragma once
-//返回向量中逆序相邻元素对的总数
-template <class T> int Vector<T>::disordere() const
+
+//判断向量是否有序
+//当且仅当返回0时向量有序
+template<class T>
+int Vector<T>::disordere() const
 {
     int n = 0;
     for (int i = 0; i < _size; ++i)
     {
-        if(_elem[i-1] > _elem[i])
+        if (_elem[i - 1] > _elem[i])
             n++;
     }
-    return n;
+    return n;//返回向量中逆序相邻元素对的总数
 }
+
 #endif //HLIB_VECTOR_DISORDERED_H
