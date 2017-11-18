a6708d8df9c4f07b9066979031562828aa62ed0d
Housz <1215123803@qq.com>
2017-11-18 11:30:11 +0800

    20171118

--- vector/vector_unsort.h
+++ vector/vector_unsort.h
@@ -34,4 +34,16 @@ void Vector<T>::unsort()
 {
     unsort(0, _size-1);
 }
+
+
+//随机置乱向量，使各元素等概率出现在各位置
+template<class T>
+void permute(Vector<T> &V)
+{
+    for (int i = V.size(); i > 0; i--)
+    {
+        swap(V[i-1], V[rand()%i]);
+    }
+}
+
 #endif //HLIB_VECTOR_UNSORT_H
